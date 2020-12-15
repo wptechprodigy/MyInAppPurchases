e0c34ae6d64f60caaba13bbaae87f4848ba05db3
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-12-15 21:25:43 +0100

    Initial Commit

---
+++ MyInAppPurchases/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyInAppPurchases
+//
+//  Created by waheedCodes on 15/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ Pods/Target Support Files/StoreFrontKit/StoreFrontKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_StoreFrontKit : NSObject
+@end
+@implementation PodsDummy_StoreFrontKit
+@end
